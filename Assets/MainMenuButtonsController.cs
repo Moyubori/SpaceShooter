50baf492a3eb1f981d2cd5754d45e31333a1ec28
Tymoteusz Buczkowski <moyubori@gmail.com>
2016-12-03 22:03:29 +0100

    Fixed ui scaling issues. Added main menu

---
+++ Assets/MainMenuButtonsController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtonsController : MonoBehaviour {
+
+	public void Play(){
+		Application.LoadLevel("shmup");
+	}
+
+	public void Options(){
+		// To be added
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+}
